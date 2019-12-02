629641c07dc62113416f426697f35c4a97919c85
Yi Chen <yc311@duke.edu>
2019-12-02 01:33:09 -0500

    trying to debug decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -58,17 +58,54 @@ public class HuffProcessor {
 	 * @param out
 	 *            Buffered bit stream writing to the output file.
 	 */
+
+	public HuffNode readTree(BitInputStream in){
+		int bit = in.readBits(1);
+		if (bit == -1) {
+			throw new HuffException("bad input");
+		}
+		if (bit == 0) {
+			HuffNode left = readTree(in);
+			HuffNode right = readTree(in);
+			return new HuffNode(0, 0, left, right);
+		} else {
+			int value = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(value, 0, null, null);
+		}
+	}
+
+
 	public void decompress(BitInputStream in, BitOutputStream out){
 
 		int magic = in.readBits(BITS_PER_INT);
 		if (magic != HUFF_TREE) {
 			throw new HuffException("invalid magic number "+magic);
 		}
-		out.writeBits(BITS_PER_INT,magic);
+		//out.writeBits(BITS_PER_INT,magic);
+		HuffNode root = readTree(in);
+		HuffNode current = root;
 		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+			int val = in.readBits(1);
+			if (val == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+
+			if (val == 0) {
+				current = current.myLeft;
+			} else {
+				current = current.myRight;
+			}
+
+			if (current.myLeft == null && current.myRight == null) {
+				if (current.myValue == PSEUDO_EOF) {
+					break;
+				} else {
+					out.writeBits(BITS_PER_WORD, val);
+					current = root;
+				}
+			}
+
+
 		}
 		out.close();
 	}
